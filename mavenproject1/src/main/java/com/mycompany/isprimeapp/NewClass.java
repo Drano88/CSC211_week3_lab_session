ade77db585deed8c044dfb0702d4cce8e75010cd
itlabs <itlabs@WHB019_12.it.farmingdale.edu>
2021-09-16 14:41:38 -0400

    Week 3

--- mavenproject1/src/main/java/com/mycompany/isprimeapp/NewClass.java
+++ mavenproject1/src/main/java/com/mycompany/isprimeapp/NewClass.java
@@ -2,14 +2,23 @@ package com.mycompany.isprimeapp;
 
 
 public class NewClass {
+
+
     boolean isPrime(int n){
         for(int i=2; i<n; i++){
             if(n%i == 0) 
-            {
-                return false;
-            }  
-        }
+   // boolean isPrime(int n){
         return true;
+    }
+
+    int getPrimeSum(int[] nums){
+        int sm=0;
+        for(int i=0; i<nums.length; i++){
+            if(isPrime(nums[i])){
+                sm+=nums[i];
+            }
+        }
+        return sm;
     }
 
     public static void main(String[] args) {
@@ -18,5 +27,10 @@ public class NewClass {
         System.out.println("Number "+x+" is " +(
                 nc.isPrime(x)?"":" not a "
                 )+ " prime "   );
+        int[] arr={3,4,8,7,9,10,12,5,11};
+        int sum= nc.getPrimeSum(arr);
+        System.out.println("The sum of the primes in the array is "+ sum );
+
+
     }
 }
